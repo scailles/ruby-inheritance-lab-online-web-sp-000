bdc8841a99a3f31957d01bf8fda09c2c085cd999
Shaina C <scailles23@gmail.com>
2021-01-10 07:03:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+  
+  attr_accessor :knowledge
+  
+  
+def initialize
+  @first_name
+  @last_name
+  @knowledge = []
+end
+
+
+def learn(knowledge)
+  @knowledge << knowledge
+end
+  
 
 end
